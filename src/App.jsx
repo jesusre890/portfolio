62815a87e5e766dfecf706dd59721681a1ae100f
jesusre890 <jesusre890@gmail.com>
2023-08-16 18:00:45 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <h1>Jesús Re</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-red-700'>
+      <p>Soy el header</p>
+    </div>
+  )
+}
+
+export default Header
