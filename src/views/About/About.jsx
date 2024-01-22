253c33e00b939fc6412a82ad2b581456b3550228
jesusre890 <jesusre890@gmail.com>
2024-01-22 16:07:23 -0300

    nuevo cv

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Info from "../../components/InfoAbout/Info";
-import cv from "../../assets/JesúsRe-FullStackDeveloper.pdf";
+import cv from "../../assets/JesúsRe-CV-FullStackDev.pdf";
 import ButtonCv from "../../components/ButtonCv/ButtonCv";
 import './About.css'
 
