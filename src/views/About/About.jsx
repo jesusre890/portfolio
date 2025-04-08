b414eade9e5b3c5916f028525a71b8158944d5fc
jesusre890 <jesusre890@gmail.com>
2025-04-08 12:56:36 -0300

    feat/about refact

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Info from "../../components/InfoAbout/Info";
-import cv from "../../assets/CV - Jesús Re - Fullstack Developer.pdf";
+import cv from "../../assets/CV - Jesús Re - Frontend Developer.pdf";
 import ButtonCv from "../../components/ButtonCv/ButtonCv";
 import './About.css'
 
